78b330f467a5a2d3a2389a9f43005e2a2055a0c1
jaroslawberek <berekjaroslaw@gmail.com>
2023-09-02 23:49:15 +0200

    start

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'vendor/autoload.php';
+use Jaropackeds\Boxess\Box;
+ 
+
+$class = new Box();
---
+++ src/Boxess/Box.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Jaropackeds\Boxess;
+
+class Box{
+    public function __construct()
+    {
+        echo "Jestem jebanym Boxem z pakietu jaropackeds boxess!";
+    }
+}
